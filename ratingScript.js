b2aa8f121d9c40fef81a178e05ab1bb8f9e5abb5
Sebastian Dziechciarz <sebastian.dziechciarz@wp.pl>
2020-09-26 16:33:21 +0200

    resize main

--- ratingScript.js
+++ ratingScript.js
@@ -6,16 +6,9 @@ btn.addEventListener("click", () => {
     modal.classList.add("modal_active");
 });
 
-// exitBtn.addEventListener("click", () => {
-//     modal.classList.remove("modal_active");
-// });
-
 const wrong = document.querySelector(".wrong");
-// const exitBtn = document.querySelector("modal_star");
 const btnWrong = document.querySelector(".thumb_third");
 
 btnWrong.addEventListener("click", () => {
-    console.log("ok");
-    console.log(wrong);
     wrong.classList.add("wrong_active");
 });
